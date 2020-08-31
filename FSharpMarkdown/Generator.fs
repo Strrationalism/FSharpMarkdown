c3a19895dc6b45fc20c2feb79783f3bcb47b24d6
Seng Jik <853974536@qq.com>
2020-08-31 09:12:09 +0800

    gen html

--- FSharpMarkdown/Generator.fs
+++ FSharpMarkdown/Generator.fs
@@ -104,3 +104,98 @@ let markdown (doc: Document) =
     |> Seq.map Markdown.mdElement
     |> Seq.reduce (fun a b -> a + newLine + b)
 
+module private Html =
+    let htmlTextElement = function
+    | Plain t -> t
+    | Italic t -> sprintf "<i>%s</i>" t
+    | Bold t -> sprintf "<strong>%s</strong>" t
+    | ItalicBold t -> sprintf "<i><strong>%s</strong></i>" t
+    | Strike t -> sprintf "<s>%s</s>" t
+    | Hyperlink (t,href) -> sprintf "<a href=\"%s\">%s</a>" href t
+    | Directlink t -> sprintf "<a href=\"%s\">%s</a>" t t
+    | Image (alt,src) -> sprintf "<img alt=\"%s\" src=\"%s\" />" alt src
+    | InlineCode c -> sprintf "<code>%s</code>" c
+
+    let htmlText x = x |> Seq.map htmlTextElement |> Seq.reduce (fun a b -> a + " " + b)
+
+    let rec htmlList x =
+        let wrapper =
+            match x.listType with
+            | Ordered -> sprintf "<ol>%s%s</ol>%s" newLine
+            | Unordered -> sprintf "<ul>%s%s</ul>%s" newLine
+        x.items
+        |> Seq.map (fun (t,child) ->
+            let child = 
+                match child with
+                | None -> ""
+                | Some x -> htmlList x
+            sprintf "<li>%s</li>%s" (htmlText t + child) newLine)
+        |> Seq.reduce (+)
+        |> fun x -> wrapper x newLine
+
+    let rec htmlTasklist x =
+        x.items
+        |> Seq.map (fun (finished, t, child) -> 
+            let child = 
+                match child with
+                | Some x -> htmlTasklist x
+                | None -> ""
+            let chk = if finished then "checked" else ""
+            sprintf "<p><input type=\"checkbox\" disabled=\"disabled\" %s/>%s</p>%s" 
+                chk (htmlText t + child) newLine)
+        |> Seq.reduce (+)
+
+    let htmlTable x =
+        let (header,align) =
+            x.header
+            |> Seq.map (fun (text,align) -> htmlText text, align)
+            |> Seq.toArray
+            |> Array.unzip
+        let align = 
+            align
+            |> Array.map (function
+            | Left -> "left"
+            | Center -> "center"
+            | Right -> "right")
+        let header =
+            header
+            |> Array.mapi (fun i -> sprintf "<th align=\"%s\">%s</th>" align.[i])
+            |> Array.reduce (+)
+            |> fun x -> sprintf "<tr>%s</tr>%s" x newLine
+
+        let content =
+            x.content
+            |> Seq.map (fun x -> 
+                x
+                |> Seq.mapi (fun i x ->
+                    htmlText x |> sprintf "<td align=\"%s\">%s</td>" align.[i])
+                |> Seq.reduce (+)
+                |> fun x -> sprintf "<tr>%s</tr>%s" x newLine)
+            |> Seq.reduce (+)
+        
+        sprintf "<table>%s%s%s</table>%s" newLine header content newLine
+
+    let htmlElement = function
+    | Heading (level,x) ->
+        if level >= 1 && level <= 6 then
+            sprintf "<h%d>%s</h%d>%s" level (htmlText x) level newLine
+        else raise (MarkdownException "Heading level must between 1 to 6.")
+    | Code (_,c) -> sprintf "<code>%s%s%s</code>%s" newLine c newLine newLine
+    | Separator -> sprintf "<hr/>%s" newLine
+    | List ls -> htmlList ls
+    | TaskList ls -> htmlTasklist ls
+    | Table t -> htmlTable t
+    | BlockQuote (level,t) -> 
+        let leftWrapper = String.init level (fun _ -> "<r>")
+        let rightWrapper = String.init level (fun _ -> "</r>")
+        sprintf "%s%s%s%s%s%s" 
+            leftWrapper newLine (htmlText t) newLine rightWrapper newLine
+    | Paragraph p ->
+        sprintf "<p>%s%s%s</p>%s" newLine (htmlText p) newLine newLine
+
+let html (doc: Document) =
+    doc
+    |> Seq.map Html.htmlElement
+    |> Seq.reduce (fun a b -> a + newLine + b)
+
+
